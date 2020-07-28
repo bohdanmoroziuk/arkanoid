2edc0fffbeae1a76838be21ec7b74813038616ab
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-28 13:40:35 +0300

    feat: allow the paddle to hit the ball

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -55,9 +55,13 @@ function draw() {
   if (y + dy < ballRadius) {
     dy = -dy;
   } else if (y + dy > canvas.height - ballRadius) {
-    alert('Game Over');
-    location.reload();
-    clearInterval(interval);
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert('Game Over');
+      location.reload();
+      clearInterval(interval);
+    }
   }
 
   if (rightPressed) {
